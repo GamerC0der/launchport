df4f4f9a41c760985b5b5e6f6aa499a181987521
GamerC0der <159653318+GamerC0der@users.noreply.github.com>
2025-11-12 13:01:36 -0800

    LaunchPort Text

--- nextjs/app/page.tsx
+++ nextjs/app/page.tsx
@@ -1,6 +1,13 @@
+function NameOf() {
+  return null;
+}
+
 export default function Home() {
   return (
-    <div>
+    <div className="flex justify-center items-center min-h-screen">
+      <div className="tab text-center text-4xl">
+        LaunchPort
+      </div>
     </div>
   );
 }
